f09d60e49b1369ac33ba1dc5fb8b2fde1de8f8bd
taku.shiraishi <tshiraishi@unisrv.jp>
2024-04-01 17:31:15 +0900

    新規作成コミット

--- force-app/main/default/lwc/quotationSchedule/quotationSchedule.js
+++ force-app/main/default/lwc/quotationSchedule/quotationSchedule.js
@@ -1,4 +1,4 @@
-import { LightningElement , wire} from 'lwc';
+import { LightningElement } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import { createRecord } from 'lightning/uiRecordApi';
 import { reduceErrors } from 'c/ldsUtils';
@@ -6,51 +6,74 @@ import si_QuotationSchedule__c_OBJECT from '@salesforce/schema/si_QuotationSched
 import ScheduleName__c_FIELD from '@salesforce/schema/si_QuotationSchedule__c.ScheduleName__c';
 import EndDate__c_FIELD from '@salesforce/schema/si_QuotationSchedule__c.EndDate__c';
 import StartDate__c_FIELD from '@salesforce/schema/si_QuotationSchedule__c.StartDate__c';
-import { getPicklistValues, getObjectInfo } from 'lightning/uiObjectInfoApi';
 
 
 export default class QuotationSchedule extends LightningElement {
 
-    @wire(getObjectInfo, { objectApiName: si_QuotationSchedule__c_OBJECT })
-    si_QuotationSchedule__cObjectInfo;
-
-    @wire(getPicklistValues, { 
-            recordTypeId: '$si_QuotationSchedule__cObjectInfo.data.defaultRecordTypeId', 
-            fieldApiName: ScheduleName__c_FIELD })
-            ScheduleName__cOptions;
-
-
             
-    si_QuotationSchedule__cId;
+    // si_QuotationSchedule__cId;
     ScheduleName__c = '';
     EndDate__c = '';
     StartDate__c = '';
+    value = '';
 
-    get options() {
-        return [
-            { label: '要件定義', value: '要件定義' },
-            { label: '基本設計', value: '基本設計' },
-            { label: 'PG', value: 'PG' },
-            { label: 'テスト', value: 'テスト' },
-            { label: '連携テスト', value: '連携テスト' },
-        ];
-    }
+    // options() {
+    //     return [
+    //         { label: '要件定義', value: '要件定義' },
+    //         { label: '基本設計', value: '基本設計' },
+    //         { label: 'PG', value: 'PG' },
+    //         { label: 'テスト', value: 'テスト' },
+    //         { label: '連携テスト', value: '連携テスト' },
+    //     ];
+    // }
 
+        Options = [
+        { value: '要件定義', label: '要件定義' },
+        {
+            value: '基本設計',
+            label: '基本設計',
+        },
+        {
+            value: 'PG',
+            label: 'PG',
+        },
+
+        {
+            value: 'テスト',
+            label: 'テスト',
+        },
+        
+        {
+            value: '連携テスト',
+            label: '連携テスト',
+        },
+    ];
 
     
     handleNameChange(event) {
-        this.quotationScheduleId = undefined;
-        this.ScheduleName__c = event.target.value;
+        // this.quotationScheduleId = undefined;
+        // this.ScheduleName__c = event.target.value;
         this.EndDate__c = event.target.value;
-        this.StartDate__c = event.target.value;
+        // this.StartDate__c = event.target.value;
+        // this.ScheduleName__c = event.detail.value;
     }
 
+    handleName2Change(event) {
+        this.ScheduleName__c = event.detail.value;
+    }
+
+    handleName3Change(event) {
+        this.StartDate__c = event.target.value;
+    }
+    // value = '';
+
     async createsi_QuotationSchedule__c() {
-        const fields = {};
+        const fields = {}
         fields[ScheduleName__c_FIELD.fieldApiName] = this.ScheduleName__c;
         fields[EndDate__c_FIELD.fieldApiName] = this.EndDate__c;
         fields[StartDate__c_FIELD.fieldApiName] = this.StartDate__c;
         const recordInput = { apiName: si_QuotationSchedule__c_OBJECT.objectApiName, fields };
+        
         try {
             const quotationSchedule = await createRecord(recordInput);
             this.QuotationScheduleId = quotationSchedule.id;
@@ -69,6 +92,8 @@ export default class QuotationSchedule extends LightningElement {
                     variant: 'error'
                 })
             );
+            
         }
     }
 }
+
